0bc00dc793b5d7510c9be58875928127f17ea260
Kevin Yu <kevinyu567@gmail.com>
2024-01-10 11:06:14 -0500

    Remove more boilerplate

--- app/classes/page.tsx
+++ app/classes/page.tsx
@@ -11,12 +11,12 @@ export default async function ClassesPage() {
     const classes = await loadClasses();
 
     return (
-        <div className="container py-24">
+        <main className="container py-24">
             <h1 className="text-4xl font-bold mb-3">
                 Classes
             </h1>
 
             <Classes classes={classes} />
-        </div>
+        </main>
     )
 }
