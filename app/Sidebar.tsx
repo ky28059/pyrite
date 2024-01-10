a188642e1060799515412412082a7ea59057e82f
Kevin Yu <kevinyu567@gmail.com>
2024-01-10 14:15:08 -0500

    Fix sidebar size

--- app/Sidebar.tsx
+++ app/Sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarItem from '@/app/SidebarItem';
 
 export default function Sidebar() {
     return (
-        <aside className="pl-3 py-24 border-r border-tertiary dark:border-tertiary-dark flex flex-col sticky top-0 h-screen gap-1 bg-gray-100 dark:bg-content-secondary-dark">
+        <aside className="w-[12rem] flex-none pl-3 py-24 border-r border-tertiary dark:border-tertiary-dark flex flex-col sticky top-0 h-screen gap-1 bg-gray-100 dark:bg-content-secondary-dark">
             <SidebarItem href="/">Home</SidebarItem>
             <SidebarItem href="/classes">Classes</SidebarItem>
             <SidebarItem href="/preferences">Preferences</SidebarItem>
