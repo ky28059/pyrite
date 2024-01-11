11403ea7894604dd0b6525c2dd37946fc40c39a4
Kevin Yu <kevinyu567@gmail.com>
2024-01-11 14:54:00 -0500

    Fix viewdate affecting next period bar

--- app/(home)/Schedule.tsx
+++ app/(home)/Schedule.tsx
@@ -8,19 +8,37 @@ import HomeDatePicker from '@/app/(home)/HomeDatePicker';
 import Calendar from '@/app/(home)/Calendar';
 import ClassIndicator from '@/app/(home)/ClassIndicator';
 
-// Utils
+// Contexts
 import UserDataContext from '@/contexts/UserDataContext';
+import CurrentTimeContext from '@/contexts/CurrentTimeContext';
+
+// Utils
 import {parseUnitimeMinutes} from '@/hooks/useNextPeriod';
 import type {Section} from '@/util/unitime';
 
 
 export default function Schedule(props: {classes: {[id: string]: Section}}) {
-    const {data} = useContext(UserDataContext);
     const [viewDate, setViewDate] = useState(DateTime.now().startOf('day'));
 
-    // Filter classes by weekday, sort by start time
+    const {data} = useContext(UserDataContext);
+    const time = useContext(CurrentTimeContext)
+
+    // Filtered `viewDate` classes by weekday
     const filtered = data.courseIds.map((id) => props.classes[id]).filter((c) => {
         switch (viewDate.weekday) {
+            case 1: return c.dayOfWeek.includes('M');
+            case 2: return /T(?!h)/.test(c.dayOfWeek);
+            case 3: return c.dayOfWeek.includes('W');
+            case 4: return c.dayOfWeek.includes('Th');
+            case 5: return c.dayOfWeek.includes('F');
+        }
+        return false;
+    });
+
+    // Sorted current-day classes for progress indicator
+    // TODO: better name for this and above perhaps
+    const sorted = data.courseIds.map((id) => props.classes[id]).filter((c) => {
+        switch (time.weekday) {
             case 1: return c.dayOfWeek.includes('M');
             case 2: return /T(?!h)/.test(c.dayOfWeek);
             case 3: return c.dayOfWeek.includes('W');
@@ -37,7 +55,7 @@ export default function Schedule(props: {classes: {[id: string]: Section}}) {
                 setViewDate={setViewDate}
             />
 
-            <ClassIndicator classes={filtered} />
+            <ClassIndicator classes={sorted} />
             <Calendar classes={filtered} />
         </div>
     )
