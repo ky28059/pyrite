a9134104aea626965efa63b80e9380e23139fc46
Kevin Yu <kevinyu567@gmail.com>
2024-02-08 20:25:34 -0500

    Test events route

--- app/api/events/[date]/route.ts
+++ app/api/events/[date]/route.ts
@@ -8,11 +8,12 @@ import type {EventsResponse} from '@/util/boilerlink';
  * format in the path params.
  */
 export async function GET(request: Request, {params}: {params: {date: string}}) {
-    console.log(params.date);
-
     // Query only for events that start *on* the requested day.
     const rangeStart = DateTime.fromISO(params.date);
     const rangeEnd = rangeStart.plus({days: 1});
+
+    console.log(params.date, rangeStart.toISO(), rangeEnd.toISO());
+    console.log(`https://boilerlink.purdue.edu/api/discovery/event/search?startsAfter=${rangeStart.toISO()}&startsBefore=${rangeEnd.toISO()}&orderByField=startsOn&orderByDirection=ascending&status=Approved&take=100&query=`);
 
     const res: EventsResponse = await (await fetch(`https://boilerlink.purdue.edu/api/discovery/event/search?startsAfter=${rangeStart.toISO()}&startsBefore=${rangeEnd.toISO()}&orderByField=startsOn&orderByDirection=ascending&status=Approved&take=100&query=`)).json()
     return NextResponse.json(res);
