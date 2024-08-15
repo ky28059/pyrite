e19f94e6c077e9980c25f8902df1667fb99c2772
Kevin Yu <kevinyu567@gmail.com>
2024-08-15 12:41:47 -0700

    Revert "Fix type error again :skull:"
    This reverts commit 0daadea31f58886dbc8b304ab5294c6f5eec80fc.

---
+++ components/ClassesProvider.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import type { ReactNode } from 'react';
+import ClassesContext, { Classes } from '@/contexts/ClassesContext';
+
+
+export default function ClassesProvider(props: { children: ReactNode, classes: Classes }) {
+    return (
+        <ClassesContext.Provider value={props.classes}>
+            {props.children}
+        </ClassesContext.Provider>
+    )
+}
