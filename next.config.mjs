0025f6237acf8b207e638e827de098ec96516631
Kevin Yu <kevinyu567@gmail.com>
2026-01-11 17:48:37 -0500

    Use webpack for builds, add `~offline` SW route

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import { spawnSync } from "node:child_process";
 import withSerwistInit from "@serwist/next";
 
+
+const revision = spawnSync("git", ["rev-parse", "HEAD"], { encoding: "utf-8" }).stdout ?? crypto.randomUUID();
+
 const withSerwist = withSerwistInit({
+    additionalPrecacheEntries: [{ url: "/~offline", revision }],
+
     swSrc: "app/sw.ts",
     swDest: "public/sw.js",
     disable: process.env.NODE_ENV !== 'production',
