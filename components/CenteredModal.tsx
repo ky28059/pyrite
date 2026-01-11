867c4b7b0d5c70bf9d54e20c911250d7ae3fa4b6
Kevin Yu <kevinyu567@gmail.com>
2026-01-11 17:45:22 -0500

    Update modal component, `luxon`

--- components/CenteredModal.tsx
+++ components/CenteredModal.tsx
@@ -1,45 +1,33 @@
-import { Fragment, ReactNode } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
+import type { ReactNode } from 'react';
+import { Dialog, DialogBackdrop, DialogPanel } from '@headlessui/react';
 
 
 // A reusable component to wrap a transition and dialog overlay around a screen-centered div.
-// https://github.com/GunnWATT/watt/blob/main/client/src/components/layout/CenteredModal.tsx
 type CenteredModalProps = {
-    isOpen: boolean, setIsOpen: (isOpen: boolean) => void,
-    className: string, children: ReactNode
+    isOpen: boolean,
+    setIsOpen: (open: boolean) => void,
+    className: string,
+    children: ReactNode
 }
+
 export default function CenteredModal(props: CenteredModalProps) {
-    const { isOpen, setIsOpen, className, children } = props;
-
     return (
-        <Transition show={isOpen} as={Fragment}>
-            <Dialog onClose={() => setIsOpen(false)} className="fixed z-[9999] inset-0 flex items-center justify-center">
-                <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="fixed inset-0 bg-black/40" />
-                </Transition.Child>
+        <Dialog
+            className="fixed z-[9999] inset-0 flex items-center justify-center"
+            open={props.isOpen}
+            onClose={() => props.setIsOpen(false)}
+        >
+            <DialogBackdrop
+                transition
+                className="fixed inset-0 bg-black/40 transition duration-300 data-closed:duration-200 ease-out data-closed:ease-in data-closed:opacity-0"
+            />
 
-                <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100 scale-100"
-                    leaveTo="opacity-0 scale-95"
-                >
-                    <Dialog.Panel className={className}>
-                        {children}
-                    </Dialog.Panel>
-                </Transition.Child>
-            </Dialog>
-        </Transition>
+            <DialogPanel
+                transition
+                className={props.className + ' transition duration-300 data-closed:duration-200 ease-out data-closed:ease-in data-closed:scale-95 data-closed:opacity-0'}
+            >
+                {props.children}
+            </DialogPanel>
+        </Dialog>
     )
 }
